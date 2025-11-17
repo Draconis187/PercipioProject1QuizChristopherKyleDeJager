449be42a7110d81c46fc149c0b6a3394c878dead
Draconis187 <christopherkyledejager@gmail.com>
2025-11-17 13:49:13 +0000

    Added conditional for next and back buttons, added result images and messages for the end of the quiz, added accordion logic and made some changes to logic for better readability.

--- Ckdejager_FE_Project1_quizLogic.js
+++ Ckdejager_FE_Project1_quizLogic.js
@@ -67,7 +67,7 @@ const QuizQuestionData = [
     questionID: 7,
     questionText:
       "What is the name of the droid that was made by Anakin Skywalker when he was a child?",
-    possibleAnswers: ["R2-D2", "C3PO", "BB-8", "Chopper"],
+    possibleAnswers: ["R2-D2", "C2P0", "BB-8", "Chopper"],
     correctAnswer: "R2-D2",
   },
   {
@@ -95,33 +95,47 @@ const QuizQuestionData = [
 
 const quizData = QuizQuestionData;
 
-let userAnswers = [];
+const userAnswers = [];
+const wrongAnswers = [];
 
 const nextButton = document.getElementById("next-button");
 const backButton = document.getElementById("back-button");
 const resetButton = document.getElementById("reset-button");
+
 const questionText = document.getElementById("question-text");
 const questionHeader = document.getElementById("question-header");
-
-const answer = document.getElementsByName("quizQuestion");
-
-const quizQuestions = document.getElementsByClassName("quizQuestionsText");
+const quizQuestions = document.getElementsByName("quizQuestion");
+const quizQuestionsText = document.getElementsByClassName("quizQuestionsText");
 
 const homePage = document.getElementById("home-page");
 const quizPage = document.getElementById("quiz-page");
 const resultsPage = document.getElementById("results-page");
+
 const results = document.getElementById("results");
 const resultsText = document.getElementById("results-text");
+const retryMessage = document.getElementById("retry-message");
+
+const quizSummaryBlock = document.getElementById("quiz-summary");
+const accordions = document.getElementsByClassName("accordion");
+const accordionPanels = document.getElementsByClassName("panel");
+const userAnswer = document.getElementById("user-answer");
+const correctAnswer = document.getElementById("correct-answer");
 
 let currentQuestionID = 0;
 let quizScore = 0;
+
+for (quizQuestion in quizQuestions) {
+  quizQuestions[quizQuestion].onclick = function () {
+    nextButton.disabled = false;
+  };
+}
 
 nextButton.addEventListener("click", () => {
   if (currentQuestionID < 11) {
     if (currentQuestionID === 0) {
       currentQuestionID++;
     } else {
-      checkAnswer(answer);
+      checkAnswer(quizQuestions);
       currentQuestionID++;
     }
     updatePage(currentQuestionID);
@@ -129,7 +143,13 @@ nextButton.addEventListener("click", () => {
 });
 
 backButton.addEventListener("click", () => {
-  if (currentQuestionID > 1) {
+  if (currentQuestionID === 1) {
+    if (window.confirm("If you go back, the quiz will be reset. Continue?")) {
+      currentQuestionID--;
+      updatePage(currentQuestionID);
+      restartQuiz(currentQuestionID);
+    }
+  } else if (currentQuestionID > 1) {
     currentQuestionID--;
     updatePage(currentQuestionID);
   }
@@ -144,21 +164,36 @@ function updatePage(currentQuestionID) {
   if (currentQuestionID > 0 && currentQuestionID < 11) {
     homePage.hidden = true;
     resultsPage.hidden = true;
+    nextButton.disabled = true;
     updateQuestion(currentQuestionID);
   } else if (currentQuestionID === 0) {
     homePage.hidden = false;
+    quizPage.hidden = true;
     resultsPage.hidden = true;
   } else if (currentQuestionID === 11) {
     resultsPage.hidden = false;
     quizPage.hidden = true;
     displayResults(currentQuestionID);
+    if (wrongAnswers.length > 0) {
+      quizSummaryBlock.hidden = false;
+      quizSummary();
+    } else {
+      quizSummaryBlock.hidden = true;
+    }
   } else {
     quizPage.hidden = true;
   }
 
-  for (let i = 0; i < answer.length; i++) {
-    if (answer[i].checked === true) {
-      answer[i].checked = false;
+  for (let i = 0; i < quizQuestions.length; i++) {
+    if (
+      quizQuestions[i].checked === true &&
+      quizQuestions[i].value != userAnswers[currentQuestionID - 1]
+    ) {
+      quizQuestions[i].checked = false;
+      //nextButton.disabled = true;
+    } else if (quizQuestions[i].value === userAnswers[currentQuestionID - 1]) {
+      quizQuestions[i].checked = true;
+      nextButton.disabled = false;
     }
   }
 
@@ -174,15 +209,32 @@ function updateQuestion(currentQuestionID) {
     questionHeader.innerHTML = `Question ${currentQuestionID}`;
     questionText.innerHTML = quizData[currentQuestionID - 1].questionText;
 
-    for (let i = 0; i < answer.length; i++) {
-      quizQuestions[i].innerHTML =
+    for (let i = 0; i < quizQuestions.length; i++) {
+      quizQuestionsText[i].innerHTML =
         quizData[currentQuestionID - 1].possibleAnswers[i];
-      answer[i].value = quizData[currentQuestionID - 1].possibleAnswers[i];
+      quizQuestions[i].value =
+        quizData[currentQuestionID - 1].possibleAnswers[i];
+    }
+
+    if (userAnswers[currentQuestionID - 1]) {
+      for (let i = 0; i < quizQuestions.length; i++) {
+        if (quizQuestions[i].value === userAnswers[currentQuestionID - 1]) {
+          quizQuestions[i].checked = true;
+        } else {
+          quizQuestions[i].checked = false;
+        }
+      }
+      updateButtons(currentQuestionID);
     }
   }
 }
 
 function updateButtons(currentQuestionID) {
+  if (currentQuestionID === 0) {
+    nextButton.disabled = false;
+    backButton.hidden = true;
+  }
+
   if (currentQuestionID === 1) {
     backButton.innerHTML = "Return to Home Page";
     backButton.hidden = false;
@@ -203,9 +255,9 @@ function updateButtons(currentQuestionID) {
   }
 }
 
-function checkAnswer(answer) {
-  for (let i = 0; i < answer.length; i++) {
-    if (answer[i].checked === true) {
+function checkAnswer(quizQuestions) {
+  for (let i = 0; i < quizQuestions.length; i++) {
+    if (quizQuestions[i].checked === true) {
       let selectedAnswer = document.querySelector(
         'input[name="quizQuestion"]:checked'
       ).value;
@@ -229,6 +281,10 @@ function checkAnswer(answer) {
       } else {
         if (selectedAnswer === quizData[currentQuestionID - 1].correctAnswer) {
           quizScore++;
+        } else if (
+          selectedAnswer != quizData[currentQuestionID - 1].correctAnswer
+        ) {
+          wrongAnswers.push(currentQuestionID - 1);
         }
         userAnswers.push(selectedAnswer);
       }
@@ -238,27 +294,51 @@ function checkAnswer(answer) {
 
 function displayResults() {
   results.innerHTML = `${quizScore} out of 10`;
+
+  if (quizScore < 10) {
+    retryMessage.innerHTML =
+      "If you would like to try and get a higher score, hit 'Restart Quiz'!";
+  } else retryMessage.innerHTML = "Congrats on getting the top score!";
+
   switch (quizScore) {
     case 0:
-      resultsText.innerHTML = "Pathetic";
+      resultsText.innerHTML = "You have failed me for the last time";
+      document.getElementById("results-image").src =
+        "./QuizImages/StarWarsDarthVaderFailForTheLastTime.png";
+      document.getElementById("results-image").alt =
+        "Darth Vader you have failed me for the last time";
       break;
     case 1:
     case 2:
     case 3:
       resultsText.innerHTML = "I find your lack of score disturbing";
+      document.getElementById("results-image").src =
+        "./QuizImages/DarthVaderIFindYourLackOfScoreDisturbing.jpg";
+      document.getElementById("results-image").alt =
+        "Darth Vader I find your lack of score disturbing";
       break;
     case 4:
     case 5:
-      resultsText.innerHTML = "Your score is on par with a Stormtrooper";
+      resultsText.innerHTML = "Your score is on par with a Stormtrooper's aim";
+      document.getElementById("results-image").src =
+        "./QuizImages/StormTrooperAim.PNG";
+      document.getElementById("results-image").alt = "Stormtrooper firing";
       break;
     case 6:
     case 7:
       resultsText.innerHTML = "Adequate performance for a rebel";
+      document.getElementById("results-image").src =
+        "./QuizImages/RebelAllianceGroupPicture.jpeg";
+      document.getElementById("results-image").alt =
+        "Rebels in a corridor with Han Solo, Leia Organa and Chewbacca";
       break;
     case 8:
     case 9:
     case 10:
       resultsText.innerHTML = "You have brought balance to the Force";
+      document.getElementById("results-image").src =
+        "./QuizImages/TheChosenOne.png";
+      document.getElementById("results-image").alt = "The Chosen One";
       break;
   }
 }
@@ -266,5 +346,67 @@ function displayResults() {
 function restartQuiz(currentQuestionID) {
   quizScore = 0;
   userAnswers.length = 0;
+  wrongAnswers.length = 0;
+  while (quizSummaryBlock.hasChildNodes()) {
+    quizSummaryBlock.removeChild(quizSummaryBlock.firstChild);
+  }
   updatePage(currentQuestionID);
+}
+
+function questionAlreadySelected(currentQuestionID) {
+  let i = 0;
+  nextButton.disabled = true;
+  while (i < quizQuestions.length) {
+    i++;
+    if (quizQuestions[i].checked != false) {
+      nextButton.disabled = false;
+      break;
+    }
+  }
+}
+
+function quizSummary() {
+  const questionSummaryHeaderText = document.createElement("h4");
+  questionSummaryHeaderText.innerHTML = "Quiz Summary";
+  quizSummaryBlock.appendChild(questionSummaryHeaderText);
+
+  for (let i = 0; i < wrongAnswers.length; i++) {
+    const questionSummaryHeader = document.createElement("button");
+    questionSummaryHeader.setAttribute("id", `Question ${wrongAnswers[i] + 1}`);
+    questionSummaryHeader.classList.add("accordion");
+    questionSummaryHeader.innerText = `Question ${wrongAnswers[i] + 1}`;
+
+    const questionSummaryBody = document.createElement("div");
+    questionSummaryBody.classList.add("panel");
+
+    let questionSummaryQuestionText = document.createElement("p");
+    questionSummaryQuestionText.innerHTML = `Question asked: ${
+      quizData[wrongAnswers[i]].questionText
+    }`;
+    questionSummaryBody.appendChild(questionSummaryQuestionText);
+
+    let questionSummaryUserAnswer = document.createElement("p");
+    questionSummaryUserAnswer.innerHTML = `You answered: ${
+      userAnswers[wrongAnswers[i]]
+    }`;
+    questionSummaryBody.appendChild(questionSummaryUserAnswer);
+
+    let questionSummaryCorrectAnswer = document.createElement("p");
+    questionSummaryCorrectAnswer.innerHTML = `Correct answer is: ${
+      quizData[wrongAnswers[i]].correctAnswer
+    }`;
+    questionSummaryBody.appendChild(questionSummaryCorrectAnswer);
+
+    questionSummaryHeader.addEventListener("click", () => {
+      questionSummaryBody.classList.toggle("active");
+      if (questionSummaryBody.style.display === "block") {
+        questionSummaryBody.style.display = "none";
+      } else {
+        questionSummaryBody.style.display = "block";
+      }
+    });
+
+    quizSummaryBlock.appendChild(questionSummaryHeader);
+    quizSummaryBlock.appendChild(questionSummaryBody);
+  }
 }
